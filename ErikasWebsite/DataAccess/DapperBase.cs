391c3b711b35f9fc35bf1355d8bf335603fc1035
ChaseHansen <chase.hansen10@gmail.com>
2015-10-16 17:51:53 -0600

    Initial Commit

---
+++ ErikasWebsite/DataAccess/DapperBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public abstract class DapperBase
+    {
+        private static string _connect = ConfigurationManager.ConnectionStrings["ErikasWebsite"].ConnectionString;
+
+        public static string ConnectionString
+        {
+            get
+            {
+                return _connect;
+            }
+            set
+            {
+                _connect = value;
+            }
+        }
+        protected static SqlConnection Open(string connectionstring = null)
+        {
+            var con = new SqlConnection(connectionstring ?? _connect);
+            con.Open();
+            return con;
+        }
+    }
+}
